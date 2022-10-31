96e2c0106affab7a0e271adf6d29ab5b4d0081bb
ss99x2002 <ss99x200211@gmail.com>
2022-10-31 22:17:33 +0900

    [UPD] 저장 Custom Dialog 구현 완료

--- app/src/main/java/com/example/assignment4/memo/MemoActivity.kt
+++ app/src/main/java/com/example/assignment4/memo/MemoActivity.kt
@@ -6,7 +6,9 @@ import android.text.Editable
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
+import android.view.WindowManager
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.assignment4.R
@@ -16,29 +18,12 @@ import com.example.assignment4.ui.dialog.CustomDialog
 import java.time.LocalDate
 import java.util.*
 
-class MemoActivity: AppCompatActivity() ,View.OnClickListener{
-    private lateinit var binding:ActivityMemoBinding
+class MemoActivity: AppCompatActivity() ,View.OnClickListener {
+    private lateinit var binding: ActivityMemoBinding
     private lateinit var textVar: Editable
-    private lateinit var titleVar : Editable
+    private lateinit var titleVar: Editable
     private lateinit var dlg: CustomDialog
 
-    override fun onClick(v: View?) {
-        when(v?.id)
-        {
-            R.id.btn_save->{
-                with(binding)
-                {
-                    if (etTitle.text==null)
-                    {
-                        Toast.makeText(this@MemoActivity,"제목을 입력하세요!",Toast.LENGTH_SHORT).show()
-                    }
-                    else{
-                        showSaveDialog()
-                    }
-                }
-            }
-        }
-    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMemoBinding.inflate(layoutInflater)
@@ -46,81 +31,123 @@ class MemoActivity: AppCompatActivity() ,View.OnClickListener{
         initView()
     }
 
-    private fun initView()
-    {
+    private fun initView() {
         with(binding)
         {
             tvDay.text = (LocalDate.now()).toString()
         }
     }
 
-
-    override fun onRestart() {
-        super.onRestart()
-        showBackDialog()
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btn_save -> {
+                with(binding)
+                {
+                    if (etTitle.text.isEmpty()) {
+                        Toast.makeText(this@MemoActivity, "제목을 입력하세요!", Toast.LENGTH_SHORT).show()
+                    } else {
+                        showSaveDialog()
+                    }
+                }
+            }
+        }
     }
 
     override fun onStop() {
         super.onStop()
+        Log.e("summer","onStop")
         with(binding)
         {
-            textVar = etMemo.text
             titleVar = etTitle.text
+            textVar = etMemo.text
             etMemo.text.clear()
             etTitle.text.clear()
         }
     }
 
-    private fun showBackDialog()
-    {
-        Log.e("summer","back 버튼 눌려짐")
-        dlg = CustomDialog(this@MemoActivity,"이어서 작성하시겠습니까?").apply{
-            this.setClickListener(object: CustomDialog.DialogClickListener{
-                override fun onConfirm() {
-                    with(binding)
-                    {
-                        etMemo.text = textVar
-                        etTitle.text= titleVar
-                    }
-                }
-                override fun onClose() {
-                    textVar.clear()
-                    titleVar.clear()
-                    with(binding)
-                    {
-                        etMemo.text.clear()
-                        etTitle.text.clear()
-                    }
-                }
-            })
-        }
-        dlg.show()
+
+    override fun onRestart() {
+        Log.e("summer","onRestart")
+        super.onRestart()
+        showDialog()
     }
-    private fun showSaveDialog()
+
+    private fun showDialog()
     {
-        Log.e("summer","save dialog로 들어옴")
+        val dialog = AlertDialog.Builder(this)
+        dialog.setTitle("메모")
+            .setMessage("이어서 작성하시겠습니까?")
+            .setPositiveButton("예") {dialog, which ->
+                binding.etMemo.text = textVar
+            }
+            .setNegativeButton("아니오") {dialog,which ->
+                textVar.clear()
+                binding.etMemo.text= null
+            }
+            .create()
+            .show()
+    }
+
+    private fun showBackDialog() {
+        Log.e("summer", "back 버튼 눌려짐")
         with(binding)
         {
-            dlg = CustomDialog(this@MemoActivity,"메모를 저장하시겠습니까?").apply{
-                this.setClickListener(object : CustomDialog.DialogClickListener{
-                    override fun onConfirm() {
-                        Log.e("summer","save dialog on Confirm으로 들어옴")
-                        val mIntent = Intent(this@MemoActivity,MemoListActivity::class.java).apply{
-                            putExtra("title","${etTitle.text}")
-                            putExtra("note","${etMemo.text}")
-                            putExtra("day","${tvDay.text}")
-                        }
-                        setResult(RESULT_OK,mIntent)
-                        if (!isFinishing) finish()
-                    }
+           DataBindingUtil.inflate<DialogConfirmBinding>(LayoutInflater.from(this@MemoActivity),R.layout.dialog_confirm,null,false).apply {
+               this.dialog = CustomDialog(this@MemoActivity,root,"이어서 작성하시겠습니까?").apply{
+                   this.setClickListener(object : CustomDialog.DialogClickListener {
+                       override fun onConfirm() {
+                           etMemo.text = textVar
+                           etTitle.text= titleVar
+                       }
+                       override fun onClose() {
+                           textVar.clear()
+                           titleVar.clear()
+                           with(binding)
+                           {
+                               etMemo.text.clear()
+                               etTitle.text.clear()
+                           }
+                       }
 
-                    override fun onClose() {
-                        Log.e("summer","save dialog on Close로 들어옴")
-                    }
-                })
-            }
-            dlg.show()
+                   })
+                   show()
+               }
+           }
         }
+    }
 
+    private fun showSaveDialog() {
+        Log.e("summer", "save dialog로 들어옴")
+
+        with(binding)
+        {
+            DataBindingUtil.inflate<DialogConfirmBinding>(
+                LayoutInflater.from(this@MemoActivity),
+                R.layout.dialog_confirm,
+                null,
+                false
+            ).apply {
+                this.dialog = CustomDialog(this@MemoActivity, root, "메모를 저장하시겠습니까?").apply {
+                    this.setClickListener(object : CustomDialog.DialogClickListener {
+                        override fun onConfirm() {
+                            Log.e("summer", "save dialog on Confirm으로 들어옴")
+                            val mIntent =
+                                Intent(this@MemoActivity, MemoListActivity::class.java).apply {
+                                    putExtra("title", "${etTitle.text}")
+                                    putExtra("note", "${etMemo.text}")
+                                    putExtra("day", "${tvDay.text}")
+                                }
+                            setResult(RESULT_OK, mIntent)
+                            if (!isFinishing) finish()
+                        }
+
+                        override fun onClose() {
+                            Log.e("summer", "save dialog on Close로 들어옴")
+                        }
+                    })
+                    show()
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/assignment4/ui/dialog/CustomDialog.kt
+++ app/src/main/java/com/example/assignment4/ui/dialog/CustomDialog.kt
@@ -5,28 +5,36 @@ import android.content.Context
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.view.Window
 import android.view.WindowManager
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import com.example.assignment4.R
+import com.example.assignment4.databinding.DialogConfirmBinding
 
-class CustomDialog(context: Context,text:String,width:Int = WindowManager.LayoutParams.MATCH_PARENT, height:Int = WindowManager.LayoutParams.WRAP_CONTENT):Dialog(context) {
+class CustomDialog(context: Context,view:View,text:String):Dialog(context) {
     private lateinit var listener: DialogClickListener
     private var isClickConfirm:Boolean = false
 
     init{
         setCanceledOnTouchOutside(true)
-        setContentView(R.layout.dialog_confirm)
+        setContentView(view)
         findViewById<TextView>(R.id.tv_dialog_title).text = text
-        window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT)) //바깥 투명하게인듯
-        window?.setLayout(width,height)
+        window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))//바깥 투명하게인듯
+        window?.setLayout(WindowManager.LayoutParams.WRAP_CONTENT,WindowManager.LayoutParams.WRAP_CONTENT)
     }
-
     interface DialogClickListener{
         fun onClose()
         fun onConfirm()
     }
 
+    override fun onStart() {
+        super.onStart()
+
+    }
     fun setClickListener(listener_ : DialogClickListener)
     {
         this.listener = listener_
